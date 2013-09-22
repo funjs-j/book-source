91d01f28aece456d18c34314f9c8615ecbc55be8
Functional JavaScript (Japanese translation) <funjs-j@users.noreply.github.com>
2013-09-22 13:53:51 +0900

    Japanese ver. initial

--- chapter05.js
+++ chapter05.js
@@ -1,317 +1,238 @@
+function dispatch(/* 任意の数の関数 */) {
+	var funs = _.toArray(arguments);
+	var size = funs.length;
 
-function dispatch(/* funs */) {
-  var funs = _.toArray(arguments);
-  var size = funs.length;
+	return function(target /*, 追加の引数 */) {
+		var ret = undefined;
+		var args = _.rest(arguments);
 
-  return function(target /*, args */) {
-    var ret = undefined;
-    var args = _.rest(arguments);
+		for (var funIndex = 0; funIndex < size; funIndex++) {
+			var fun = funs[funIndex];
+			ret = fun.apply(fun, construct(target, args));
 
-    for (var funIndex = 0; funIndex < size; funIndex++) {
-      var fun = funs[funIndex];
-      ret = fun.apply(fun, construct(target, args));
+			if (existy(ret)) return ret;
+		}
 
-      if (existy(ret)) return ret;
-    }
-
-    return ret;
-  };
+		return ret;
+	};
 }
 
 var str = dispatch(invoker('toString', Array.prototype.toString),
-invoker('toString', String.prototype.toString));
-
-str("a");
-//=> "a"
-
-str(_.range(10));
-//=> "0,1,2,3,4,5,6,7,8,9"
+							invoker('toString', String.prototype.toString));
 
 function stringReverse(s) {
-  if (!_.isString(s)) return undefined;
-  return s.split('').reverse().join("");
+	if (!_.isString(s))
+		return undefined;
+	return s.split('').reverse().join("");
 }
 
-stringReverse("abc");
-//=> "cba"
-
-stringReverse(1);
-//=> undefined
-
-var rev = dispatch(invoker('reverse', Array.prototype.reverse),
-stringReverse);
-
-rev([1,2,3]);
-//=> [3, 2, 1]
-
-rev("abc");
-//=> "cba"
-
-function performCommandHardcoded(command) {
-  var result;
-
-  switch (command.type) 
-  {
-  case 'notify':
-    result = notify(command.message);
-    break;
-  case 'join':
-    result = changeView(command.target);
-    break;
-  default:
-    alert(command.type);
-  }
-
-  return result;
-}
+var polyrev = dispatch(invoker('reverse', Array.prototype.reverse), stringReverse);
+var sillyReverse = dispatch(polyrev, always(42));
 
 function isa(type, action) {
-  return function(obj) {
-    if (type === obj.type)
-      return action(obj);
-  }
+	return function(obj) {
+		if (type === obj.type) return action(obj);
+	}
 }
 
 var performCommand = dispatch(
-  isa('notify', function(obj) { return notify(obj.message) }),
-  isa('join',   function(obj) { return changeView(obj.target) }),
-  function(obj) { alert(obj.type) });
+	isa('notify', function(obj) { return notify(obj.message) }),
+	isa('join', function(obj) { return changeView(obj.target) }),
+	function(obj) { alert(obj.type) });
 
 var performAdminCommand = dispatch(
-  isa('kill', function(obj) { return shutdown(obj.hostname) }),
-  performCommand);
+	isa('kill', function(obj) { return shutdown(obj.hostname) }),
+	performCommand);
 
 var performTrialUserCommand = dispatch(
-  isa('join', function(obj) { alert("Cannot join until approved") }),
-  performCommand);
+	isa('join', function(obj) { alert("許可されるまで参加できません") }),
+	performCommand);
 
 function rightAwayInvoker() {
-  var args = _.toArray(arguments);
-  var method = args.shift();
-  var target = args.shift();
-
-  return method.apply(target, args);
+	var args = _.toArray(arguments);
+	var method = args.shift();
+	var target = args.shift();
+	
+	return method.apply(target, args);
 }
 
-rightAwayInvoker(Array.prototype.reverse, [1,2,3])
-//=> [3, 2, 1]
-
 function leftCurryDiv(n) {
-  return function(d) {
-    return n/d;
-  };
+	return function(d) {
+		return n/d;
+	};
 }
 
 function rightCurryDiv(d) {
-  return function(n) {
-    return n/d;
-  };
+	return function(n) {
+		return n/d;
+	};
 }
 
+var divide10By = leftCurryDiv(10);
+var divideBy10 = rightCurryDiv(10);
+
 function curry(fun) {
-  return function(arg) {
-    return fun(arg);
-  };
+	return function(arg) {
+		return fun(arg);
+	};
 }
 
 function curry2(fun) {
-  return function(secondArg) {
-    return function(firstArg) {
-        return fun(firstArg, secondArg);
-    };
-  };
+	return function(secondArg) {
+		return function(firstArg) {
+			return fun(firstArg, secondArg);
+		};
+	};
 }
 
 function div(n, d) { return n / d }
 
 var div10 = curry2(div)(10);
 
-div10(50);
-//=> 5
-
 var parseBinaryString = curry2(parseInt)(2);
 
-parseBinaryString("111");
-//=> 7
-
-parseBinaryString("10");
-//=> 2
-
 var plays = [{artist: "Burial", track: "Archangel"},
-             {artist: "Ben Frost", track: "Stomp"},
-             {artist: "Ben Frost", track: "Stomp"},
-             {artist: "Burial", track: "Archangel"},
-             {artist: "Emeralds", track: "Snores"},
-             {artist: "Burial", track: "Archangel"}];
-
-_.countBy(plays, function(song) {
-  return [song.artist, song.track].join(" - ");
-});
-
-//=> {"Ben Frost - Stomp": 2,
-//    "Burial - Archangel": 3,
-//    "Emeralds - Snores": 1}
+					{artist: "Ben Frost", track: "Stomp"},
+					{artist: "Ben Frost", track: "Stomp"},
+					{artist: "Burial", track: "Archangel"},
+					{artist: "Emeralds", track: "Snores"},
+					{artist: "Burial", track: "Archangel"}];
 
 function songToString(song) {
-  return [song.artist, song.track].join(" - ");
+	return [song.artist, song.track].join(" - ");
 }
 
 var songCount = curry2(_.countBy)(songToString);
 
-songCount(plays);
-//=> {"Ben Frost - Stomp": 2,
-//    "Burial - Archangel": 3,
-//    "Emeralds - Snores": 1}
-
 function curry3(fun) {
-  return function(last) {
-    return function(middle) {
-      return function(first) {
-        return fun(first, middle, last);
-      };
-    };
-  };
+	return function(last) {
+		return function(middle) {
+			return function(first) {
+				return fun(first, middle, last);
+			};
+		};
+	};
 };
 
 var songsPlayed = curry3(_.uniq)(false)(songToString);
 
-songsPlayed(plays);
-
-//=> [{artist: "Burial", track: "Archangel"},
-//    {artist: "Ben Frost", track: "Stomp"},
-//    {artist: "Emeralds", track: "Snores"}]
-
 function toHex(n) {
-  var hex = n.toString(16);
-  return (hex.length < 2) ? [0, hex].join(''): hex;
+	var hex = n.toString(16);
+	return (hex.length < 2) ? [0, hex].join(''): hex;
 }
 
 function rgbToHexString(r, g, b) {
-  return ["#", toHex(r), toHex(g), toHex(b)].join('');
+	return ["#", toHex(r), toHex(g), toHex(b)].join('');
 }
 
-rgbToHexString(255, 255, 255);
-//=> "#ffffff"
-
-var blueGreenish = curry3(rgbToHexString)(255)(200);
-
-blueGreenish(0);
-//=> "#00c8ff"
 
 var greaterThan = curry2(function (lhs, rhs) { return lhs > rhs });
-var lessThan    = curry2(function (lhs, rhs) { return lhs < rhs });
+var lessThan = curry2(function (lhs, rhs) { return lhs < rhs });
 
 var withinRange = checker(
-  validator("arg must be greater than 10", greaterThan(10)),
-  validator("arg must be less than 20", lessThan(20)));
+	validator("10より大きい必要があります", greaterThan(10)),
+	validator("20より小さい必要があります", lessThan(20)));
 
 function divPart(n) {
-  return function(d) {
-    return n / d;
-  };
+	return function(d) {
+		return n/d;
+	};
 }
 
 var over10Part = divPart(10);
-over10Part(2);
-//=> 5
 
 function partial1(fun, arg1) {
-  return function(/* args */) {
-    var args = construct(arg1, arguments);
-    return fun.apply(fun, args);
-  };
+	return function(/* args */) {
+		var args = construct(arg1, arguments);
+		return fun.apply(fun, args);
+	};
 }
+
+function partial1native(fun, arg1) {
+	return fun.bind(undefined, arg1);
+}
+
+var over10Part1 = partial1(div, 10);
 
 function partial2(fun, arg1, arg2) {
-  return function(/* args */) {
-    var args = cat([arg1, arg2], arguments);
-    return fun.apply(fun, args);
-  };
+	return function(/* args */) {
+		var args = cat([arg1, arg2], arguments);
+		return fun.apply(fun, args);
+	};
 }
 
-var div10By2 = partial2(div, 10, 2)
+var div10By2 = partial2(div, 10, 2);
 
-div10By2()
-//=> 5
 
 function partial(fun /*, pargs */) {
-  var pargs = _.rest(arguments);
+	var pargs = _.rest(arguments);
 
-  return function(/* arguments */) {
-    var args = cat(pargs, _.toArray(arguments));
-    return fun.apply(fun, args);
-  };
+	return function(/* arguments */) {
+		var args = cat(pargs, _.toArray(arguments));
+		return fun.apply(fun, args);
+	};
 }
 
-var zero = validator("cannot be zero", function(n) { return 0 === n
-});
-var number = validator("arg must be a number", _.isNumber);
+var div10By2By4By5000Partial = partial(div, 10, 2, 4, 5000);
+
+var zero = validator("0ではいけません", function(n) { return 0 === n });
+var number = validator("引数は数値である必要があります", _.isNumber);
 
 function sqr(n) {
-  if (!number(n)) throw new Error(number.message);
-  if (zero(n))    throw new Error(zero.message);
-
-  return n * n;
+	if (!number(n)) throw new Error(number.message);
+	if (zero(n)) throw new Error(zero.message);
+	return n * n;
 }
 
 function condition1(/* validators */) {
-  var validators = _.toArray(arguments);
+	var validators = _.toArray(arguments);
 
-  return function(fun, arg) {
-    var errors = mapcat(function(isValid) {
-      return isValid(arg) ? [] : [isValid.message];
-    }, validators);
+	return function(fun, arg) {
+		var errors = mapcat(function(isValid) {
+			return isValid(arg) ? [] : [isValid.message];
+		}, validators);
 
-    if (!_.isEmpty(errors))
-      throw new Error(errors.join(", "));
+		if (!_.isEmpty(errors))
+			throw new Error(errors.join(", "));
 
-    return fun(arg);
-  };
+		return fun(arg);
+	};
 }
 
 var sqrPre = condition1(
-  validator("arg must not be zero", complement(zero)),
-  validator("arg must be a number", _.isNumber));
+	validator("0ではいけません", complement(zero)),
+	validator("引数は数値である必要があります", _.isNumber));
 
 function uncheckedSqr(n) { return n * n };
-
-uncheckedSqr('');
-//=> 0
 
 var checkedSqr = partial1(sqrPre, uncheckedSqr);
 
 var sillySquare = partial1(
-  condition1(validator("should be even", isEven)),
+  condition1(validator("偶数を入力してください", isEven)),
   checkedSqr);
 
 var validateCommand = condition1(
-  validator("arg must be a map", _.isObject),
-  validator("arg must have the correct keys", hasKeys('msg',
-'type')));
+  validator("マップデータである必要があります", _.isObject),
+  validator("設定オブジェクトは正しいキーを持っている必要があります", hasKeys('msg', 'type')));
 
 var createCommand = partial(validateCommand, _.identity);
 
 var createLaunchCommand = partial1(
   condition1(
-    validator("arg must have the count down", hasKeys('countDown'))),
+    validator("設定オブジェクトはcountDownが必要です", hasKeys('countDown'))),
   createCommand);
+  
+function isntString(str) {
+  return !_.isString(str);
+}
 
 var isntString = _.compose(function(x) { return !x }, _.isString);
 
-isntString([]);
-//=> true
-
-function not(x) { return !x }
-
 var composedMapcat = _.compose(splat(cat), _.map);
 
-composedMapcat([[1,2],[3,4],[5]], _.identity);
-//=> [1, 2, 3, 4, 5]
-
 var sqrPost = condition1(
-  validator("result should be a number", _.isNumber),
-  validator("result should not be zero", complement(zero)),
-  validator("result should be positive", greaterThan(0)));
-
-var megaCheckedSqr = _.compose(partial(sqrPost, _.identity),
-checkedSqr);
+  validator("結果は数値である必要があります", _.isNumber),
+  validator("結果はゼロではない必要があります", complement(zero)),
+  validator("結果は正の数である必要があります", greaterThan(0)));
+  
+var megaCheckedSqr = _.compose(partial(sqrPost, _.identity), checkedSqr);
