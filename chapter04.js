9c96fd4db22de7a8aa4e6f5bb3a27ff2a6a0de27
Functional JavaScript (Japanese translation) <funjs-j@users.noreply.github.com>
2013-09-22 13:52:17 +0900

    Japanese ver. initial

--- chapter04.js
+++ chapter04.js
@@ -1,215 +1,156 @@
 var people = [{name: "Fred", age: 65}, {name: "Lucy", age: 36}];
 
-_.max(people, function(p) { return p.age });
-
-//=> {name: "Fred", age: 65}
-
 function finder(valueFun, bestFun, coll) {
-  return _.reduce(coll, function(best, current) {
-    var bestValue = valueFun(best);
-    var currentValue = valueFun(current);
-
-    return (bestValue === bestFun(bestValue, currentValue)) ? best : current;
-  });
+	return _.reduce(coll, function(best, current) {
+		var bestValue = valueFun(best);
+		var currentValue = valueFun(current);
+		return (bestValue === bestFun(bestValue, currentValue)) ? best : current;
+	});
 }
 
 function best(fun, coll) {
-  return _.reduce(coll, function(x, y) {
-    return fun(x, y) ? x : y;
-  });
+	return _.reduce(coll, function(x, y) {
+		return fun(x, y) ? x : y;
+	});
 }
-
-best(function(x,y) { return x > y }, [1,2,3,4,5]);
-//=> 5
 
 function repeat(times, VALUE) {
-  return _.map(_.range(times), function() { return VALUE; });
+	return _.map(_.range(times), function() { return VALUE; });
 }
-
-repeat(4, "Major");
-//=> ["Major", "Major", "Major", "Major"]
 
 function repeatedly(times, fun) {
-  return _.map(_.range(times), fun);
+	return _.map(_.range(times), fun);
 }
 
-repeatedly(3, function() {
-  return Math.floor((Math.random()*10)+1);
-});
-//=> [1, 3, 8]
-
 function iterateUntil(fun, check, init) {
-  var ret = [];
-  var result = fun(init);
-
-  while (check(result)) {
-    ret.push(result);
-    result = fun(result);
-  }
-
-  return ret;
+	var ret = [];
+	var result = fun(init);
+	while (check(result)) {
+		ret.push(result);
+		result = fun(result);
+	}
+	return ret;
 };
 
 function always(VALUE) {
-  return function() {
-    return VALUE;
-  };
+	return function() {
+		return VALUE;
+	};
 };
 
 function invoker (NAME, METHOD) {
-  return function(target /* args ... */) {
-    if (!existy(target)) fail("Must provide a target");
-
-    var targetMethod = target[NAME];
-    var args = _.rest(arguments);
-
-    return doWhen((existy(targetMethod) && METHOD === targetMethod), function() {
-      return targetMethod.apply(target, args);
-    });
-  };
+	return function(target /* args ... */) {
+		if (!existy(target)) fail("Must provide a target");
+		var targetMethod = target[NAME];
+		var args = _.rest(arguments);
+		return doWhen((existy(targetMethod) && METHOD === targetMethod), function() {
+			return targetMethod.apply(target, args);
+		});
+	};
 };
 
 var rev = invoker('reverse', Array.prototype.reverse);
 
-_.map([[1,2,3]], rev);
-//=> [[3,2,1]]
-
-function uniqueString(len) {
-  return Math.random().toString(36).substr(2, len);
-};
-
-uniqueString(10);
-//=> "3rm6ww5w0x"
+var add100 = makeAdder(100);
 
 function uniqueString(prefix) {
-  return [prefix, new Date().getTime()].join('');
+	return [prefix, new Date().getTime()].join('');
 };
-
-uniqueString("argento");
-//=> "argento1356107740868"
 
 function makeUniqueStringFunction(start) {
-  var COUNTER = start;
-
-  return function(prefix) {
-    return [prefix, COUNTER++].join('');
-  }
+	var COUNTER = start;
+	return function(prefix) {
+		return [prefix, COUNTER++].join('');
+	}
 };
-
-var uniqueString = makeUniqueStringFunction(0);
-
-uniqueString("dari");
-//=> "dari0"
-
-uniqueString("dari");
-//=> "dari1"
 
 var generator = {
-  count: 0,
-  uniqueString: function(prefix) {
-    return [prefix, this.count++].join('');
-  }
+	count: 0,
+	uniqueString: function(prefix) {
+		return [prefix, this.count++].join('');
+	}
 };
 
-generator.uniqueString("bohr");
-//=> bohr0
-
-generator.uniqueString("bohr");
-//=> bohr1
-
 var omgenerator = (function(init) {
-  var COUNTER = init;
+	var COUNTER = init;
 
-  return {
-    uniqueString: function(prefix) {
-      return [prefix, COUNTER++].join('');
-    }
-  };
+	return {
+		uniqueString: function(prefix) {
+			return [prefix, COUNTER++].join('');
+		}
+	};
 })(0);
-
-omgenerator.uniqueString("lichking-");
-//=> "lichking-0"
 
 var nums = [1,2,3,null,5];
 
-_.reduce(nums, function(total, n) { return total * n });
-//=> 0
-
 function fnull(fun /*, defaults */) {
-  var defaults = _.rest(arguments);
-
-  return function(/* args */) {
-    var args = _.map(arguments, function(e, i) {
-      return existy(e) ? e : defaults[i];
-    });
-
-    return fun.apply(null, args);
-  };
+	var defaults = _.rest(arguments);
+	return function(/* args */) {
+		var args = _.map(arguments, function(e, i) {
+			return existy(e) ? e : defaults[i];
+		});
+		return fun.apply(null, args);
+	};
 };
+
 
 var safeMult = fnull(function(total, n) { return total * n }, 1, 1);
 
-_.reduce(nums, safeMult);
-//=> 30
-
-function defaults(d) {
-  return function(o, k) {
-    var val = fnull(_.identity, d[k]);
-    return o && val(o[k]);
+function defaults(df) {
+  return function(obj, key) {
+    var val = fnull(_.identity, df[key]);
+    return obj && val(obj[key]);
   };
 }
 
 function doSomething(config) {
-  var lookup = defaults({critical: 108});
-
-  return lookup(config, 'critical');
+	var lookup = defaults({critical: 108});
+	return lookup(config, 'critical');
 }
-
-doSomething({critical: 9});
-//=> 9
-
-doSomething({});
-//=> 108
 
 function checker(/* validators */) {
-  var validators = _.toArray(arguments);
-
-  return function(obj) {
-    return _.reduce(validators, function(errs, check) {
-      if (check(obj))
-        return errs;
-      else
-        return _.chain(errs).push(check.message).value();
-    }, []);
-  };
+	var validators = _.toArray(arguments);
+	return function(obj) {
+		return _.reduce(validators, function(errs, check) {
+			if (check(obj))
+				return errs
+			else
+				return _.chain(errs).push(check.message).value();
+		}, []);
+	};
 }
+
+var alwaysPasses = checker(always(true), always(true));
+
+var fails = always(false);
+fails.message = "人生における過ち";
+var alwaysFails = checker(fails);
 
 function validator(message, fun) {
-  var f = function(/* args */) {
-    return fun.apply(fun, arguments);
-  };
-
-  f['message'] = message;
-  return f;
+	var f = function(/* args */) {
+		return fun.apply(fun, arguments);
+	};
+	f['message'] = message;
+	return f;
 }
+
+var gonnaFail = checker(validator("ZOMG!", always(false)));
 
 function aMap(obj) {
-  return _.isObject(obj);
+	return _.isObject(obj);
 }
-
-var checkCommand = checker(validator("must be a map", aMap));
 
 function hasKeys() {
-  var KEYS = _.toArray(arguments);
+	var KEYS = _.toArray(arguments);
+	
+	var fun = function(obj) {
+		return _.every(KEYS, function(k) {			return _.has(obj, k);
+		});
+	};
 
-  var fun = function(obj) {
-    return _.every(KEYS, function(k) {
-      return _.has(obj, k);
-    });
-  };
-
-  fun.message = cat(["Must have values for keys:"], KEYS).join(" ");
-  return fun;
+	fun.message = cat(["これらのキーが存在する必要があります："], KEYS).join(" ");
+	return fun;
 }
 
-var checkCommand = checker(validator("must be a map", aMap), hasKeys('msg', 'type'));
+var checkCommand = checker(validator("マップデータである必要があります", aMap),
+										hasKeys('msg', 'type'));
